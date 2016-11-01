28673eb1ffad4be6bce22dc6c7859dfc470cc7fd
Fabio Souto <fabiolimasouto@gmail.com>
2016-11-01 20:30:37 -0200

    add ParamContext class

---
+++ FlSouto/ParamContext.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace FlSouto;
+
+class ParamContext extends \ArrayObject{
+
+	protected $params = [];
+
+	function param($param){
+		if($param instanceof Param){
+			// ...
+		} else if(is_string($param)) {
+			if(isset($this->params($param))){
+				return $this->params[$param];
+			}
+			$param = new Param($param);
+		} else {
+			throw new \InvalidArgumentException("Parameter must be string or an instance of Param.");
+		}
+		$param->context($this);
+		$this->params[$param->name()] = $param;
+		return $param;
+	}
+
+	function process(){
+		$output = [];
+		$errors = [];
+		foreach($this->params as $param){
+			$result = $param->process($this);
+			$output[$param->name()] = $result->output;
+			if($error = $result->error){
+				$errors[$param->name()] = $result->error;
+			}
+		}
+		$result = new ParamContextResult();
+		$result->output = $output;
+		$result->errors = $errors;
+		return $result;
+	}
+
+}
+
+class ParamContextResult{
+
+	var $output = [];
+	var $errors = [];
+
+}
