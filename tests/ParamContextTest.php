e36fc6acf1f5629834f1fa6a37bf874857549d13
Fabio Souto <fabiolimasouto@gmail.com>
2016-11-01 22:07:20 -0200

    add tests for ParamContext class

---
+++ tests/ParamContextTest.php
@@ -0,0 +1,69 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+require_once('vendor/autoload.php');
+
+use FlSouto\ParamContext;
+use FlSouto\Param;
+
+class ParamContextTest extends TestCase{
+
+	
+	function testCreation(){
+		$context = new ParamContext([
+			'user_id' => 5
+		]);
+		$user_id = $context['user_id'];
+		$this->assertEquals(5, $user_id);
+	}
+
+	function testParamRegistry(){
+
+		$context = new ParamContext();
+		$param = $context->param('lang_code');
+
+		$this->assertEquals('lang_code', $param->name());
+
+	}
+
+	function testParamReturnsSameInstance(){
+		$context = new ParamContext();
+		$param = Param::get('lang_code');
+		$context->param($param);
+
+		$this->assertEquals($context->param('lang_code'), $param);
+	}
+
+	function testDataProcessing(){
+
+		$context = new ParamContext();
+		$context->param('user_id');
+		$context->param('lang_code')->filters()->trim();
+
+		$context['user_id'] = 5;
+		$context['lang_code'] = 'en ';
+
+		$result = $context->process();
+
+		$this->assertEquals(['user_id'=>5,'lang_code'=>'en'],$result->output);
+
+	}
+
+	function testIndividualParamProcessing(){
+		
+		$context = new ParamContext();
+		$context->param('lang_code')->filters()->trim();
+
+		$context['lang_code'] = 'en ';
+
+		$result = $context->param('lang_code')->process();
+
+		$this->assertEquals('en',$result->output);
+
+	}
+
+}
+
+
+
+
